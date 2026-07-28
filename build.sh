5f412727d29a3d68c5d5f5c6bb9e9ebd97099e36
spab1967 <128197731+spab1967@users.noreply.github.com>
2026-07-28 16:35:31 -0300

    Run security checks before deployment

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ set -o errexit
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m pip install -r requirements-render.txt
+python -m pip check
+python manage.py check --deploy --fail-level ERROR
 python manage.py collectstatic --noinput
 python manage.py migrate
